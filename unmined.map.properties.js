9365c24bb05e23ac1708849504cee8406f7376b0
grim <grim@localhost>
2026-08-22 01:47:46 -0400

    Map render 2026-08-22

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -6,7 +6,7 @@ var UnminedMapProperties = {
     imageFormat: "jpeg",
     minRegionX: -18,
     minRegionZ: -28,
-    maxRegionX: 35,
+    maxRegionX: 54,
     maxRegionZ: 25,
     worldName: "Bedrock level",
     background: "#000000",
