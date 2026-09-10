384b3024e63cd09e75c2cd680136c8b4a506a063
grim <grim@localhost>
2026-09-10 01:46:07 -0400

    Map render 2026-09-10

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -4,7 +4,7 @@ var UnminedMapProperties = {
     maxZoom: 3,
     defaultZoom: 0,
     imageFormat: "jpeg",
-    minRegionX: -18,
+    minRegionX: -27,
     minRegionZ: -28,
     maxRegionX: 59,
     maxRegionZ: 25,
